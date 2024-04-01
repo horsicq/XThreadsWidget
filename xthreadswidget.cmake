66bb0739dc931ca60b0f0b7e5f5bd723e81ccd05
horsicq <horsicq@gmail.com>
2024-04-01 20:10:01 +0200

    Add new file(s): 2024-04-01

---
+++ xthreadswidget.cmake
@@ -0,0 +1,7 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+set(XTHREADSWIDGET_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/xthreadswidget.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/xthreadswidget.h
+    ${CMAKE_CURRENT_LIST_DIR}/xthreadswidget.ui
+)
